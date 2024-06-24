aaaf39ef80a8f7bfed8cbeb9307dbc43391b7373
OmerMunk <omermunk95@gmail.com>
2024-06-24 13:10:21 +0300

    added the logic of the messages queue, with a lot of todos and logs that will be removed later

--- src/controllers/queue.controller.ts
+++ src/controllers/queue.controller.ts
@@ -5,6 +5,7 @@ export const addMessageController = async (req: Request, res: Response) => {
     try {
         const queueName = req.params.queue_name
         const message = req.body;
+        console.log(`controller sending message: ${JSON.stringify(message)}`)
         const result = await addMessageToQueue(queueName, message);
         if (result) {
             res.status(201).json({
@@ -19,12 +20,20 @@ export const addMessageController = async (req: Request, res: Response) => {
 
 export const getMessageController = async (req: Request, res: Response) => {
     try {
+        console.log('in message controller')
         const queueName = req.params.queue_name
+        console.log('queueName', queueName)
         const message = await getMessageFromQueue(queueName, 1000);
+        console.log(`message: ${message}`)
         if (message) {
             res.status(200).json({
                 success: true,
                 message
+            })
+        } else {
+            res.status(204).json({
+                success: false,
+                message: 'No message found'
             })
         }
     } catch (error: any) {
--- src/service/queue.service.ts
+++ src/service/queue.service.ts
@@ -3,10 +3,14 @@ import Queue from '../models/queue'
 const queueMap = new Map<string, Queue>();
 
 export const getMessageFromQueue = async (queueName: string, timeout: number): Promise<any> => { // todo: change to message interface
-
+    console.log(`inside getMessageFromQueue with queueName: ${queueName}`)
     const queue = queueMap.get(queueName) || new Queue(queueName);
+    console.log(`queue is ${queue}`);
     queueMap.set(queueName, queue);
-    return await queue.dequeue(timeout);
+    console.log(`queueName: ${queueName} calling dequeue`)
+    const result = await queue.dequeue(timeout);
+    console.log(`result from dequeue: ${result}`)
+    return result
 }
 
 export const addMessageToQueue = async (queueName: string, payload: any): Promise<boolean> => {
