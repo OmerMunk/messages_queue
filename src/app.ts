f12bd850ec096cae26a96e0498e609e9755691ac
OmerMunk <omermunk95@gmail.com>
2024-06-24 13:08:02 +0300

    added more packages for the app - general middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
 import express, {Application, Request, Response, NextFunction} from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from "cookie-parser";
+import helmet from 'helmet'
+import cors from 'cors'
 import {indexRouter} from "./routes/index.router";
 
 
